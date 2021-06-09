190b4bbb7d7fbfe4e47ff30cc6667821ff8c89d5
dedalus-3 <van9shka357@gmail.com>
2021-06-09 17:51:55 +0500

    add comments

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -8,3 +8,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             pass
+    # add coments
